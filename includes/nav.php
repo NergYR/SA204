33c80abfc0a6ddff1e753edb13dc1458936924fb
Energetiq <energetiq@outlook.com>
2024-06-13 11:26:14 +0200

    Energetiq: Update CSS styles for responsive design

--- includes/nav.php
+++ includes/nav.php
@@ -7,15 +7,15 @@ if(isset($_COOKIE['id'])){
 
 <div class="header-bg">
         <?php if(isset($_COOKIE['id'])){
-            echo "<div class='header-button-center'><a class='header-button-right-logout' href='login/logout.php'>Deconnexion</a></div>";
+            echo "<div class='header-button-center'><a class='header-button-right-logout' href='/SA204/login/logout.php'>Deconnexion</a></div>";
             echo "<div class='header-overlay-left'></div>";
-            echo "<div class='header-overlay-right'></div>";
+            echo "<div class='header-overlay-right'><a class='home-link' href='/SA204'>Home</a></div>";
             
             }else{
-                echo "<div class='header-button-right'><a class='header-button-right-login' href='login/'>Login</a></div>";
-                echo "<div class='header-button-left'><a class='header-button-left-register' href='register/'>Register</a></div>";
+                echo "<div class='header-button-right'><a class='header-button-right-login' href='/SA204/login/'>Login</a></div>";
+                echo "<div class='header-button-left'><a class='header-button-left-register' href='/SA204/register/'>Register</a></div>";
                 echo "<div class='header-overlay-left'></div>";
-                echo "<div class='header-overlay-right'></div>";
+                echo "<div class='header-overlay-right'>  <a class='home-link' href='/SA204'>Home</a></div>";
             }?>
 </div>
 
